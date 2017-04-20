7516c40243bf3b645323939c0267c160ceff2084
amiralipor <amir.alipor140@gmail.com>
2017-04-20 09:00:11 +0430

    Create config.lua

---
+++ data/config.lua
@@ -0,0 +1,29 @@
+do local _ = {
+  admins = {},
+  disabled_channels = {},
+  enabled_plugins = {
+    "banhammer",
+    "groupmanager",
+    "msg-checks",
+    "plugins",
+    "tools",
+    "fun",
+    "info",
+    "del",
+    "nerkh",
+    "pic",
+    "ping",
+    "time1",
+    "write",
+    "addkick"
+  },
+  info_text = "》AMIR V.1\n\n\n》://github.com/amiralipor/amir.git\n\n》Admins :\n》@Professortelegram 《\n\9\9\n》\n",
+  moderation = {
+    data = "./data/moderation.json"
+  },
+  sudo_users = {
+    340026281
+  }
+}
+return _
+end
