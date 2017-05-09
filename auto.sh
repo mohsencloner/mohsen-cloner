631a7001b8314810427c8122f13291ba5d85c980
Telesuperior <Telesuperior@users.noreply.github.com>
2017-05-09 08:36:28 +0430

    Delete auto.sh

--- auto.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-COUNTER=1
-while(true) do
-./amir.sh
-curl "https://api.telegram.org/bot[ToKeN]/sendmessage" -F "chat_id=340026281" -F "text=#NEWCRASH-Reloaded-${COUNTER}-times"
-let COUNTER=COUNTER+1 
-done
